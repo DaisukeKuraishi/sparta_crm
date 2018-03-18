ec7143f13b40d91dc7fa18bd615e379da5ae85e7
DaisukeKuraishi <madgga@gmail.com>
2018-03-18 13:57:03 +0900

    ポストモデルまで

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,9 @@
 class CustomersController < ApplicationController
+
+before_action :set_customer, only: [:show, :edit, :destory, :update]
+
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def new
@@ -17,12 +20,9 @@ class CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
-
   end
 
   def update
-    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to @customer
     else
@@ -31,11 +31,9 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def destroy
-    @customer = Customer.find(params[:id])
     @customer.destroy
     redirect_to customers_path
   end
@@ -43,6 +41,10 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:family_name, :given_name, :email)
+    params.require(:customer).permit(:family_name, :given_name, :email, :company_id)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
